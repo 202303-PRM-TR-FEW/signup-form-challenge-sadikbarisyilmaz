e0a714f31d47483a5a3314e90eaa5c1905087e9c
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-04-14 10:26:53 +0300

    added click and keydown validation

--- script.js
+++ script.js
@@ -1,25 +1,53 @@
 const formList = document.querySelectorAll("#form")[0]
 
 form.addEventListener("submit", handleSubmit)
+form.addEventListener("click", validate)
+
+
+for (let index = 0; index < formList.length-1; index++) {
+    formList[index].addEventListener("keydown", validate)
+}
+
+function addError(elem) {
+    elem.nextElementSibling.innerHTML = `${elem.placeholder} cannot be empty`
+    elem.style.backgroundImage = "url('./images/icon-error.svg')"
+    elem.style.backgroundRepeat = "no-repeat"
+    elem.style.backgroundPositionX = "96%"
+    elem.style.backgroundPositionY = "center"
+    elem.style.border = "solid 2px red"
+}
+
+function removeError(elem) {
+    elem.nextElementSibling.innerHTML = ""
+    elem.style.backgroundImage = "none"
+    elem.style.borderColor = "inherit"
+}
+
+function validate(e) {
+
+    if(e.target.value !== undefined){
+
+        if(e.target.value === ""){
+            addError(e.target)     
+        }else if(e.target.id === "email" && !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(e.target.value)){
+            addError(e.target)     
+            form.email.nextElementSibling.innerHTML = "Looks like this is not an Email!"
+        }else if(e.target.id === "password" && !/\w{8}$/.test(e.target.value)){
+            addError(e.target)     
+            form.password.nextElementSibling.innerHTML = "Looks like this is not a Password!"
+        }else{
+            removeError(e.target)
+        }
+
+    }else{}
+}
+
+
 
 function handleSubmit(e) {
     e.preventDefault()
     console.log("submit");
 
-    function addError(elem) {
-        elem.nextElementSibling.innerHTML = `${elem.placeholder} cannot be empty`
-        elem.style.backgroundImage = "url('./images/icon-error.svg')"
-        elem.style.backgroundRepeat = "no-repeat"
-        elem.style.backgroundPositionX = "96%"
-        elem.style.backgroundPositionY = "center"
-        elem.style.border = "solid 2px red"
-    }
-
-    function removeError(elem) {
-        elem.nextElementSibling.innerHTML = ""
-        elem.style.backgroundImage = "none"
-        elem.style.borderColor = "inherit"
-    }
 
     for (let i = 0; i < formList.length - 1; i++) {
         if(formList[i].value === ""){
